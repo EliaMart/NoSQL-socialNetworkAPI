767abeca9746bc1d7b8d7a0d1be25cc0ba048725
Elia Martinez <eliahaydeemartinez@gmail.com>
2022-08-10 16:43:41 -0500

    deleted reaction model, added to thought model file

--- models /Thought.js
+++ models /Thought.js
@@ -1,5 +1,30 @@
 const { Schema, model } = require('mongoose');
 
+
+// Schema to create reaction 
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            // default:
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String, 
+            required: true, 
+        }
+    }
+)
+
+
 // Schema to create thought model 
 const thoughtSchema = new Schema (
     {
@@ -16,7 +41,8 @@ const thoughtSchema = new Schema (
         username: {
             type: String, 
             required: true, 
-        }
+        },
+        reactions: [reactionSchema],
     },
     {
         toJSON: {
